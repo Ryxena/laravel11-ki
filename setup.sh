8d3ae7f52b791d0b85f904b041866fc5d6fbb8ea
Rubben <147255793+Ryxena@users.noreply.github.com>
2024-11-14 10:18:18 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,11 +4,13 @@ composer require --dev barryvdh/laravel-ide-helper
 composer require laravel/telescope --dev
 composer install --prefer-dist --optimize-autoloader
 composer update
-php artisan conf:clear
+php artisan key:generate
+php artisan optimize:clear
+php artisan telescope:instal
+php artisan migrate
 php artisan ide-helper:generate
 php artisan ide-helper:models
 php artisan ide-helper:meta
-php artisan telescope:instal
-php artisan migrate
+php artisan ide-helper:eloquent
 php artisan ser
 echo "Done"
